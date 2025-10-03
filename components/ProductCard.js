450da653b95bb24a22193b3b0f1875cb8b118dbb
Emshtml <casadathereza1@gmail.com>
2025-10-03 13:43:40 -0300

    Create ProductCard.js

---
+++ components/ProductCard.js
@@ -0,0 +1,9 @@
+export default function ProductCard() {
+  return (
+    <div style={{ border: "1px solid #ddd", padding: "15px", borderRadius: "8px", margin: "10px" }}>
+      <h3>Nome do Produto</h3>
+      <p>Descrição simples do produto.</p>
+      <strong>R$ 00,00</strong>
+    </div>
+  );
+}
